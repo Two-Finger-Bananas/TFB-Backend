89557b03bd37b0da0a19c62563ecb15b6285d9b7
mwlordmason <mason.walker1709@gmail.com>
2023-07-06 10:08:30 -0600

    fixed conflicts

--- db/seedData.js
+++ db/seedData.js
@@ -24,24 +24,6 @@ async function createTables() {
         console.log(error);
     }
 }
-<<<<<<< HEAD
-/*async function updateGameById(gameId, updateGameObjData){
-    try {
-        const { rows } = await client.query(`
-            UPDATE games
-            SET title = $1, "publishDate" = $2, "gameDeveloper" = $3, genre = $4, platforms = $5, players = $6, "coverImg" = $7
-            WHERE "gameId" = $8
-            RETURNING *;
-          `, [ updateGameObjData.title, updateGameObjData.publishDate,updateGameObjData.gameDeveloper,updateGameObjData.genre, updateGameObjData.platforms, updateGameObjData.players,updateGameObjData.coverImg, gameId]);
-
-  if (rows.length){
-    return rows[0];
-  }
-} catch (error) {
-    console.log(error);
-}
-}*/
-=======
 
 // async function updateGameById(gameId, updateGameObjData){
 //     try {
@@ -82,7 +64,6 @@ async function updateGameById(gameId, fields = {}) {
         console.log(error)
     }
 }
->>>>>>> main
 
 async function destroyTables() {
     try {

3935387681fe39ae81817baab390e776f3a17f4d
mwlordmason <mason.walker1709@gmail.com>
2023-07-07 11:39:24 -0600

    commit

--- db/seedData.js
+++ db/seedData.js
@@ -18,7 +18,7 @@ async function createTables() {
                 "userId" SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL
-            )
+            );
         `)
     } catch (error) {
         console.log(error);

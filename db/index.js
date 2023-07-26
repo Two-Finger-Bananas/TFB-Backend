853141d7573304d6e762ac9c8ad85349fec5dc6a
mwlordmason <mason.walker1709@gmail.com>
2023-07-26 10:18:27 -0600

    commit

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const pg = require("pg")
 
-const client = new pg.Client("postgres://localhost:5432/bananalytics")
+const client = new pg.Client("postgres://bananalytics_user:swhKNEzRKlOc6yKBSuZtIcWjeWhhz0LZ@dpg-cj005318g3n4aimurmn0-a/bananalytics")
 
 module.exports = client
